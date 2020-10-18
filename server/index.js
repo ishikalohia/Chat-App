3929785ee60e2af9bed0b218d0936cb95c3b2801
Ishika <ishikalohia4@gmail.com>
2020-10-18 18:39:43 +0530

    First Commit, Join Function Added, Client & Server Connected

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("We have a new user");
+
+  socket.on("join", ({ name, room }, callback) => {
+    console.log(name, room);
+  });
+  socket.on("disconnect", () => {
+    console.log("User has left");
+  });
+});
+app.use(router);
+
+server.listen(PORT, () => console.log("App is running"));
